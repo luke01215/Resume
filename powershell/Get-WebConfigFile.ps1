8f0d27125a39428f9ae0d1f963a6c700b777e5ec
Darling, Luke <luke01215@gmail.com>
2022-06-29 07:19:16 -0400

    Closest yet

--- powershell/Get-WebConfigFile.ps1
+++ powershell/Get-WebConfigFile.ps1
@@ -1,26 +1,34 @@
 param (
-    [string]$fileName = "C:\LCD\GIT\self\Resume\powershell\web.Config"
+    [string]$fileName = "G:\GIT\self\Resume\powershell\web.Config"
 )
-
-function Print-Attributes ($node) {
-    $node_attributes = $node.attributes
-    foreach($attrbute in $node_attributes) {
-        Write-Output $attribute.Name
-        Write-Output $attribute.value
+function displayNodes ($node, $path) {
+    $xpath = $path
+    if ($node.NodeType -eq "Comment") {
     }
-}
-
-function Traverse-Nodes ($nodes) {
-    Print-Attributes $nodes
-    foreach($node in $nodes) {
+    else {
         $node_name = $node.Name
-        Write-Output $node_name
-        Traverse-Nodes($node.ChildNodes)
+        $node_type = $node.NodeType
+        $xpath = $xpath + "/" + $node_name
+        Write-Output "Type[$node_type] | Name[$node_name] | Path[$xpath]"
+    }
+
+    if ($null -ne $node.Attributes) {
+        $node_attributes = $node.Attributes
+        foreach($attribute in $node_attributes) {
+            $attribute_name = $attribute.Name
+            $attribute_value = $attribute.Value
+            $attributePath = $xpath + "/@" + $attribute_name
+            Write-Output "`tAttribute Name[$attribute_name] | Attribute Value[$attribute_value] | Path[$attributePath]"
+        }
+    }
+
+    $node_children = $node.ChildNodes
+    foreach($child in $node_children) {
+        displayNodes $child $xpath
     }
 }
 
-$nodeNames = [System.Collections.ArrayList]::new()
-[xml]$xml = Get-Content $fileName
-$xml | Get-Member
-$childNodes = $xml.ChildNodes
-Traverse-Nodes ($childNodes)
+[xml]$xml = Get-Content -Path $fileName
+$rootNode = $xml.DocumentElement
+displayNodes $rootNode
+Select-Xml -Path $fileName -XPath "/configuration/system.web/httpModules/FileAuthorization/@name" | Select-Object -ExpandProperty Node
